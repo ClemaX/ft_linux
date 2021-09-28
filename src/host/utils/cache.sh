ce6c4c86732b7280834885f5ebdda935aa5d20f8
Clement Hamada <clementhamada@protonmail.com>
2021-09-28 19:59:31 +0200

    host: cache: Fix cache utils

--- src/host/utils/cache.sh
+++ src/host/utils/cache.sh
@@ -1,4 +1,4 @@
-# Get a list of corrupted files
+# Get a list of corrupted files from the hashes given to STDIN.
 cache_check() # action [cache]
 {
   action="$1"
@@ -6,17 +6,19 @@ cache_check() # action [cache]
 
   echo "Checking cache..."
 
-  md5sum --check --quiet --ignore-missing "$cache" \
-  | grep FAILED \
-  | sed 's/\(.*\):.*/\1/' \
-  | xargs --no-run-if-empty $action
+  pushd "$cache"
+    md5sum --check --quiet --ignore-missing - \
+    | grep FAILED \
+    | sed 's/\(.*\):.*/\1/' \
+    | xargs --no-run-if-empty $action
+  popd
 }
 
 cache_list() # [cache]
 {
   cache="${1:-PWD}"
 
-  sources_md5 | cut -b 35- && echo linux-stable
+  cut -b 35- < "$cache/md5sums" && echo linux-stable
 }
 
 # Link all files in cache to
@@ -25,5 +27,5 @@ cache_link() # dst [cache]
   dst="${1}"
   cache="${2:-$PWD}"
 
-  cache_list "$cache" | xargs --no-run-if-empty -I{} ln -s {} "$dst"
+  cache_list "$cache" | xargs --no-run-if-empty -I{} ln -sv "$cache/{}" "$dst"
 }
