41fcb02e4eda10f838d029d8cac401b8f4d18438
Clément Hamada <clementhamada@protonmail.com>
2022-05-17 22:24:50 +0200

    host: root: Update mount script

--- src/host/root/mount.sh
+++ src/host/root/mount.sh
@@ -8,5 +8,5 @@ source "$SCRIPTDIR/utils/logger.sh"
 source "$SCRIPTDIR/utils/disk.sh"
 source "$SCRIPTDIR/utils/image.sh"
 
-loop_setup "$LOOP_DEV" "$IMG_DST"
+loop_setup "$IMG_DST"
 disk_mount "$LOOP_DEV" "$LFS"
