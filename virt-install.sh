b43f5735d79e4c86577018239b2dd3d37f94833d
Clement Hamada <clementhamada@protonmail.com>
2023-04-24 20:31:39 +0200

    Add virt-install script

--- virt-install.sh
+++ virt-install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+sudo virsh net-start default
+
 sudo virt-install --name=LFS \
 	--os-variant=linux2022 \
 	--vcpu=8 \
 	--ram=8192 \
-	--disk "path=$PWD/dist/disk.img" \
+	--disk "bus=sata,path=$PWD/dist/disk.img" \
 	--graphics spice \
 	--network network=default \
 	--import \
